be22ecdff87d21b1fa8f74e4c7d7fc8185013570
fathimashereen <116864071+fathimashereen@users.noreply.github.com>
2023-06-07 15:06:54 +0530

    S2

---
+++ arithmetic.sh
@@ -0,0 +1,12 @@
+echo "Enter first number:"
+read a
+echo "Enter second number:"
+read b
+sum=$((a + b))
+dif=$((a - b))
+multi=$((a * b))
+div=$((a / b))
+echo "sum:"$sum
+echo "difference:"$dif
+echo "product:"$multi
+echo "quotient:"$div
